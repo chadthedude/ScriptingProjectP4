fdf7710084ddb3324b6025a6d34eb9ade6cb8d20
chadthedude <724626@student.dusd.net>
2025-03-06 11:22:00 -0800

    added new scene & scripts
    added axis, raw axis, and dual axis scripts

---
+++ ScriptingProjectP4Moreno/Assets/Scripts/DualAxisExample.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DualAxisExample : MonoBehaviour
+{
+    public float hRange;
+    public float vRange;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float xPos = h * hRange;
+        float yPos = v * vRange;
+
+        transform.position = new Vector3(xPos, yPos, 0);
+        Debug.Log("h.ToString");
+        Debug.Log("v.ToString");
+
+    }
+}
--- ScriptingProjectP4Moreno/Assets/Scripts/KeyInput.cs
+++ ScriptingProjectP4Moreno/Assets/Scripts/KeyInput.cs
@@ -1,9 +1,8 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.UIElements;
+
 
 public class KeyInput : MonoBehaviour
 {
